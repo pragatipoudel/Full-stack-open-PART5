1f6e99f54984735c92e52eb4ac9a42192168df8a
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-25 14:02:45 -0500

    Add 4-4

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return likes.length === 0 
+    ? 0
+    : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
