475057233ab0b0a7a922932354e1c693df24d251
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-25 17:07:12 -0500

    Add 4-5

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -13,7 +13,27 @@ const totalLikes = (blogs) => {
     : likes.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let highestLike = -1
+    let highestBlog = {}
+    blogs.forEach((blog) => {
+        if (blog.likes > highestLike) {
+            highestLike = blog.likes
+            highestBlog = blog
+        }
+    })
+    const returnObject = {
+        title: highestBlog.title,
+        author: highestBlog.author,
+        likes: highestBlog.likes
+    }
+    return blogs.length === 0
+    ? 0
+    : returnObject
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
