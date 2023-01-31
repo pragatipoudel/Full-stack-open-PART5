edbd115d7f9c085308b83eb7772b4238a0c9090e
Pragati Poudel <pragati.poudel37@gmail.com>
2023-01-31 16:09:12 -0500

    Add 4-9

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -1,16 +1,48 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
 
 test('blogs are returned as json', async () => {
-    await api
+    const response = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier property of the blog is named id', async () => {
+    const resultBlogs = await api
+        .get(`/api/blogs/`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    resultBlogs.body.forEach((blog) => {
+        expect(blog.id).toBeDefined()
+
+    })
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "React patterns",
+        author: "Michael Chan",
+
+    }
+})
 afterAll(async () => {
     await mongoose.connection.close()
 })
