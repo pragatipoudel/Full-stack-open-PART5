f70bd6dc37bce04d27df57b69b6c3aa69a36d07b
Pragati Poudel <pragati.poudel37@gmail.com>
2023-02-01 09:04:44 -0500

    Add 4-10

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -40,8 +40,21 @@ test('a valid blog can be added', async () => {
     const newBlog = {
         title: "React patterns",
         author: "Michael Chan",
-
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0
     }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(b => b.title)
+    expect(contents).toContain('React patterns')
 })
 afterAll(async () => {
     await mongoose.connection.close()
