7484cab0767a045f9d228d8d14e71c95d055f8e7
Pragati Poudel <pragati.poudel37@gmail.com>
2023-02-01 18:00:03 -0500

    Add 4-18 and 4-19

--- blog-list/app.js
+++ blog-list/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 
 mongoose.set('strictQuery', false)
@@ -27,6 +28,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
