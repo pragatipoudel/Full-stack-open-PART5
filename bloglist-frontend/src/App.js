b7439aebfde0932614ec5ecf62eb2dfa61ff7385
Pragati Poudel <pragati.poudel37@gmail.com>
2023-02-03 14:14:44 -0500

    Add 5-2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -12,9 +12,21 @@ const App = () => {
 
 
   useEffect(() => {
+    if (!user) {
+      return
+    }
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [user])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -24,6 +36,11 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -60,6 +77,11 @@ const App = () => {
     </form>
 )
 
+const handleLogOut = () => {
+  window.localStorage.clear()
+  setUser(null)
+}
+
   if (user === null) {
     return (
       <div>
@@ -71,6 +93,12 @@ const App = () => {
       return (
         <div>
           <h2>blogs</h2>
+          <p>
+            {user.name} logged in
+            <button onClick={handleLogOut}>
+              Log Out
+            </button>
+          </p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
